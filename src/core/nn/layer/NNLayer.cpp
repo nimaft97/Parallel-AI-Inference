04ce94d2da04afd658e6985706eee8a5db46c7be
nimaft97 <nimafatholahi@gmail.com>
2024-10-13 21:50:03 -0700

    Separate different layers
    Dense and Conv2D were defined inside NNLayer but
    they are now separated and given different source
    and header files

--- src/core/nn/layer/NNLayer.cpp
+++ src/core/nn/layer/NNLayer.cpp
@@ -1,33 +1,5 @@
 #include "NNLayer.h"
 
-
-// ****************************************** NNLayer ****************************************** 
 NNLayer::NNLayer()
 {
-}
-
-// ****************************************** NNDense ****************************************** 
-
-NNDense::NNDense(dimType units): m_units(units), m_weights({units,}), m_biases({units,})
-{
-}
-
-NNTensor NNDense::forward(const NNTensor& input) const
-{
-    NNTensor output = NNTensor({m_units});
-    return output;
-}
-
-// ****************************************** NNConv2D ****************************************** 
-
-NNConv2D::NNConv2D(dimType filters, dimType kernel_size, dimType stride, dimType padding)
-    : m_filters(filters), m_kernel_size(kernel_size), m_stride(stride), m_padding(padding),
-      m_weights({filters, kernel_size, kernel_size}), m_biases({filters})
-{
-}
-
-NNTensor NNConv2D::forward(const NNTensor& input) const
-{
-    NNTensor output = NNTensor(input);
-    return output;
 }
--- src/core/nn/layer/NNLayer.h
+++ src/core/nn/layer/NNLayer.h
@@ -11,31 +11,4 @@ public:
     virtual NNTensor forward(const NNTensor& input) const = 0;
 };
 
-class NNDense : public NNLayer
-{
-public:
-    NNDense(dimType units);
-    NNTensor forward(const NNTensor& input) const override;
-
-private:
-    dimType m_units;
-    NNTensor m_weights;
-    NNTensor m_biases;
-};
-
-class NNConv2D : public NNLayer
-{
-public:
-    NNConv2D(dimType filters, dimType kernel_size, dimType stride, dimType padding);
-    NNTensor forward(const NNTensor& input) const override;
-
-private:
-    dimType m_filters;
-    dimType m_kernel_size;
-    dimType m_stride;
-    dimType m_padding;
-    NNTensor m_weights;
-    NNTensor m_biases;
-};
-
 #endif
