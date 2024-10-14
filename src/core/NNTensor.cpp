3204dff3bcd63b9d8920e8a8be6085251420505b
nimaft97 <nimafatholahi@gmail.com>
2024-10-13 17:38:14 -0700

    Add more to NNTensor
    Adding constructors
    Overloading () operator
    Adding getter/setter
    Adding member variables

--- src/core/NNTensor.cpp
+++ src/core/NNTensor.cpp
@@ -1,9 +1,54 @@
 #include "NNTensor.h"
 
-NNTensor::NNTensor()
+#include <numeric>
+
+NNTensor::NNTensor(const dimVec& dimensions): m_dimensions(dimensions)
+{
+    m_size = std::accumulate(dimensions.cbegin(), dimensions.cend(), 1u, std::multiplies<dimType>());
+    m_data.resize(m_size, 0.0f);
+}
+
+NNTensor::NNTensor(const NNTensor& other): 
+                      m_dimensions(other.m_dimensions)
+                    , m_size(other.m_size)
+                    , m_data(other.m_data)
 {
 }
 
 NNTensor::~NNTensor()
 {
+}
+
+float& NNTensor::operator()(const dimVec& indices)
+{
+    dimType idx = 0;
+    for (auto i = 0; i <= indices.size(); ++i)
+    {
+        idx *= m_dimensions[i];
+        idx += indices[i];
+    }
+
+    return m_data[idx];
+}
+
+const float& NNTensor::operator()(const dimVec& indices) const
+{
+    dimType idx = 0;
+    for (auto i = 0; i <= indices.size(); ++i)
+    {
+        idx *= m_dimensions[i];
+        idx += indices[i];
+    }
+
+    return m_data[idx];
+}
+
+dimVec NNTensor::getDimensions() const
+{
+    return m_dimensions;
+}
+
+dimType NNTensor::getSize() const
+{
+    return m_size;
 }
--- src/core/NNTensor.h
+++ src/core/NNTensor.h
@@ -1,11 +1,28 @@
 #ifndef NNTENSOR_H
 #define NNTENSOR_H
 
+#include <vector>
+
+typedef unsigned int dimType;
+typedef std::vector<dimType> dimVec;
+
 class NNTensor
 {
 public:
-    NNTensor();
+    NNTensor(const dimVec& dimensions);
+    NNTensor(const NNTensor& other);
     ~NNTensor();
+
+    dimVec getDimensions() const;
+    dimType getSize() const;
+
+    float& operator()(const dimVec& indices);
+    const float& operator()(const dimVec& indices) const;
+
+private:
+    std::vector<float> m_data;
+    dimVec m_dimensions;
+    dimType m_size;
 };
 
 #endif
