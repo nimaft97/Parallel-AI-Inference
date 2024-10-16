3a32cf29ed0a39b57acd3168c0e04eaa7b7330bf
nimaft97 <nimafatholahi@gmail.com>
2024-10-16 05:16:42 -0700

    completed the host code in multiplyOnGpu function

--- src/core/nn/tensor/TensorGpuOpenCL.cpp
+++ src/core/nn/tensor/TensorGpuOpenCL.cpp
@@ -60,3 +60,76 @@ cl_kernel TensorGpuOpenCL::loadKernel(const std::string& file_path, const std::s
 
     return kernel;
 }
+
+TensorGpuOpenCL TensorGpuOpenCL::multiplyOnGpu(const TensorGpuOpenCL& other) const
+{
+    const auto other_dims = other.getDimensions();
+    if (other_dims.size() != 2 || m_dimensions.size() != 2)
+    {
+        throw std::invalid_argument("Only matrices can be multiplied");
+    }
+
+    if (other_dims[0] != m_dimensions[1])
+    {
+        throw std::invalid_argument("Matrix dimensions don't match the requested multiplication");
+    }
+
+    cl_kernel kernel = loadKernel("gpu/kernels.clh", "matMul");
+
+    TensorGpuOpenCL result = TensorGpuOpenCL({m_dimensions[0], other_dims[1]});
+
+    // Create OpenCL buffers for the matrices
+    cl_int err = CL_SUCCESS;
+    cl_mem lBuffer = clCreateBuffer(m_cl_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                getSize() * sizeof(float), const_cast<void*>(static_cast<const void*>(m_data.data())), &err);
+    if (!err == CL_SUCCESS)
+    {
+        throw std::runtime_error("Error creating buffer");
+    }
+    cl_mem rBuffer = clCreateBuffer(m_cl_context, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR,
+                                other.getSize() * sizeof(float), const_cast<void*>(static_cast<const void*>(other.m_data.data())), &err);
+
+    if (!err == CL_SUCCESS)
+    {
+        throw std::runtime_error("Error creating buffer");
+    }
+    cl_mem resultBuffer = clCreateBuffer(m_cl_context, CL_MEM_WRITE_ONLY,
+                                        result.getSize() * sizeof(float), result.m_data.data(), &err);
+    if (!err == CL_SUCCESS)
+    {
+        throw std::runtime_error("Error creating buffer");
+    }
+
+    // set kernel args
+    clSetKernelArg(kernel, 0, sizeof(cl_mem), &lBuffer);
+    clSetKernelArg(kernel, 1, sizeof(cl_mem), &rBuffer);
+    clSetKernelArg(kernel, 2, sizeof(cl_mem), &resultBuffer);
+    clSetKernelArg(kernel, 3, sizeof(dimType), &m_dimensions[0]);
+    clSetKernelArg(kernel, 4, sizeof(dimType), &m_dimensions[1]);
+    clSetKernelArg(kernel, 5, sizeof(dimType), &other_dims[1]);
+
+    // todo optimize the numbers below
+    const size_t global_size[] = {32u, 32u};
+    const size_t local_size[] = {32u, 32u};
+
+    // enqueue the kernel
+    clEnqueueNDRangeKernel(m_cl_queue, kernel, 2, nullptr, global_size, local_size, 0, nullptr, nullptr);
+
+    // wait until execution of the kernel is over
+    err = clFinish(m_cl_queue);
+    if (!err == CL_SUCCESS)
+    {
+        throw std::runtime_error("Error emptying the queue");
+    }
+
+    // read the kernel's output
+    clEnqueueReadBuffer(m_cl_queue, resultBuffer, CL_TRUE, 0, result.getSize() * sizeof(float), result.m_data.data(), 0, nullptr, nullptr);
+
+    // release resources
+    clReleaseMemObject(lBuffer);
+    clReleaseMemObject(rBuffer);
+    clReleaseMemObject(resultBuffer);
+    clReleaseKernel(kernel);
+
+    return result;
+}
