2322045a1e6b1a887668db166fc2babf7ab2b552
nimaft97 <nimafatholahi@gmail.com>
2024-11-04 18:21:05 -0800

    Add main.cpp to demonstrate current MVP

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include "nn/tensor/Tensor.h"
+#include "nn/model/Model.h"
+#include "nn/layer/Dense.h"
 
 #include <iostream>
 
@@ -6,19 +8,30 @@ int main(int argc, char** argv)
 {
     std::cout << "Welcome to the Parallel AI Inference project" << std::endl;
 
-    auto t1 = Tensor<float>();
-    t1.set_host_data({1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f});
-    t1.set_dims({2, 3});
+    auto input = Tensor<float>();
+    input.set_host_data({1.0f, 2.0f, 3.0f});
+    input.set_dims({1, 3});
 
-    auto t2 = Tensor<float>();
-    t2.set_host_data({1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f});
-    t2.set_dims({3, 2});
+    auto weight = Tensor<float>();
+    weight.set_host_data({3.0f, 2.0f, 1.0f});
+    weight.set_dims({3, 1});
 
-    auto t3 = t2 + t2;
-    auto t4 = t1 * t2;
+    auto bias  = Tensor<float>();
+    bias.set_host_data({5.0f});
+    bias.set_dims({1, 1});
+    
+    auto dense = Dense();
+    dense.set_weight(weight);
+    dense.set_bias(bias);
 
-    std::cout << "t1: " << t1.to_string(true, true, true, true);
-    std::cout << "t2: " << t2.to_string(true, true, true, true);
-    std::cout << "t3: " << t3.to_string(true, true, true, true);
-    std::cout << "t4: " << t4.to_string(true, true, true, true);
+    auto model = Model();
+    model.add_layer(&dense);
+
+    auto result = model.execute(input);
+    
+
+    std::cout << "input: "    << input.to_string(true, true, true, true);
+    std::cout << "weight: "   << weight.to_string(true, true, true, true);
+    std::cout << "bias: "     << bias.to_string(true, true, true, true);
+    std::cout << "result: "   << result.to_string(true, true, true, true);
 }
